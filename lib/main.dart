b69f858f492ca8bff134fd5f7e2015a7ba5564ca
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-07-20 14:52:43 +0530

    bg color change

--- lib/main.dart
+++ lib/main.dart
@@ -14,117 +14,144 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              Card(
-                child: Container(
-                  width: 400,
-                  height: 70,
-                  child: ElevatedButton(
-                    child: null,
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red,
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: Card(
+                  child: Container(
+                    width: 400,
+                    height: 70,
+                    child: ElevatedButton(
+                      child: null,
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                      ),
+                      onPressed: () => playSound(1),
                     ),
-                    onPressed: () => playSound(1),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              
+              Expanded(
+                child: Card(
+                  child: Container(
+                    width: 400,
+                    height: 70,
+                    child: ElevatedButton(
+                      child: null,
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.orange,
+                      ),
+                      onPressed: () => playSound(2),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              
+              Expanded(
+                child: Card(
+                  child: Container(
+                    width: 400,
+                    height: 70,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.yellow,
+                      ),
+                      child: null,
+                      onPressed: () => playSound(3),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              
+              Expanded(
+                child: Card(
+                  child: Container(
+                    width: 400,
+                    height: 70,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
+                      ),
+                      child: null,
+                      onPressed: () => playSound(4),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              
+              Expanded(
+                child: Card(
+                  child: Container(
+                    width: 400,
+                    height: 70,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.lightBlueAccent,
+                      ),
+                      child: null,
+                      onPressed: () => playSound(5),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              
+              Expanded(
+                child: Card(
+                  child: Container(
+                    width: 400,
+                    height: 70,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                      ),
+                      child: null,
+                      onPressed: () => playSound(6),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              
+              Expanded(
+                child: Card(
+                  child: Container(
+                    width: 400,
+                    height: 70,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.purple,
+                      ),
+                      child: null,
+                      onPressed: () => playSound(7),
+                    ),
                   ),
                 ),
               ),
               SizedBox(
                 height: 10,
-              ),
-              Card(
-                child: Container(
-                  width: 400,
-                  height: 70,
-                  child: ElevatedButton(
-                    child: null,
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.orange,
-                    ),
-                    onPressed: () => playSound(2),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Card(
-                child: Container(
-                  width: 400,
-                  height: 70,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.yellow,
-                    ),
-                    child: null,
-                    onPressed: () => playSound(3),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Card(
-                child: Container(
-                  width: 400,
-                  height: 70,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.green,
-                    ),
-                    child: null,
-                    onPressed: () => playSound(4),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Card(
-                child: Container(
-                  width: 400,
-                  height: 70,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.lightBlueAccent,
-                    ),
-                    child: null,
-                    onPressed: () => playSound(5),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Card(
-                child: Container(
-                  width: 400,
-                  height: 70,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue,
-                    ),
-                    child: null,
-                    onPressed: () => playSound(6),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Card(
-                child: Container(
-                  width: 400,
-                  height: 70,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.purple,
-                    ),
-                    child: null,
-                    onPressed: () => playSound(7),
-                  ),
-                ),
               ),
             ],
           ),
